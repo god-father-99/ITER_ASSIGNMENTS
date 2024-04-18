a10e7e72aee9d6aca152714ead3d8ffc2f8f8dfc
god-father-99 <aditya23sahu@gmail.com>
2024-04-18 21:56:14 +0530

    Divide by Zero Exception

--- src/Error_Handling/Q9_EH.java
+++ src/Error_Handling/Q9_EH.java
@@ -1,7 +1,20 @@
 package Error_Handling;
 
+import java.util.Scanner;
+
 public class Q9_EH {
     public static void main(String[] args) {
-
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int a=sc.nextInt();
+        System.out.print("Enter 2nd num : ");
+        int b=sc.nextInt();
+        try{
+            int c=a/b;
+            System.out.println("division of a/b is : "+c);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
