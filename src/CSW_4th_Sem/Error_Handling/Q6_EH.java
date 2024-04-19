fe41b9567c086e4d193895ce2524383dc8435f4e
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 11:32:22 +0530

    Q5 NumberFormatException

---
+++ src/CSW_4th_Sem/Error_Handling/Q6_EH.java
@@ -0,0 +1,13 @@
+package CSW_4th_Sem.Error_Handling;
+
+public class Q6_EH {
+    public static void main(String[] args) {
+        String s="ad";
+        try {
+            int n = Integer.parseInt(s);
+        }
+        catch (NumberFormatException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
