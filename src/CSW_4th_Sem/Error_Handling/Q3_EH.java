9ce57cc81326dffc9aa203a6572ad2523554b3bd
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 11:30:52 +0530

    Q3 CustomNullPointerException

---
+++ src/CSW_4th_Sem/Error_Handling/Q3_EH.java
@@ -0,0 +1,20 @@
+package CSW_4th_Sem.Error_Handling;
+
+class CustomNullPointerException extends NullPointerException{
+    CustomNullPointerException(String s){
+        super(s);
+    }
+}
+public class Q3_EH {
+    public static void main(String[] args) {
+        String s=null;
+        try{
+            if(s==null) {
+                throw new CustomNullPointerException("String is Null");
+            }
+        }
+        catch (CustomNullPointerException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
