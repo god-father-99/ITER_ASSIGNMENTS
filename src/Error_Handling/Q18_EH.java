85ad840a86a88d46c18239d0da86bb210e70c2bb
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 10:06:36 +0530

    Q-18 ArrayIndexOutOfBounds Exception with Recursive Array Function

---
+++ src/Error_Handling/Q18_EH.java
@@ -0,0 +1,24 @@
+package Error_Handling;
+
+public class Q18_EH {
+    public static void arrTraveralRec(int [] arr,int idx,int n){
+        if(idx==n+1)
+            return;
+        try{
+            if(idx<n-1)
+                System.out.print(arr[idx]+" , ");
+            else
+                System.out.println(arr[idx]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        arrTraveralRec(arr,idx+1,n);
+    }
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        int idx=0;
+        int n=arr.length;
+        arrTraveralRec(arr,idx,n);
+    }
+}
