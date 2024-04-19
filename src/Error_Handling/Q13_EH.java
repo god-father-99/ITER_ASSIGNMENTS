f5bb29dab478000075c14cf704f33ca16c2cbd03
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 07:48:19 +0530

    Q-13 Arithmatic Exception

---
+++ src/Error_Handling/Q13_EH.java
@@ -0,0 +1,14 @@
+package Error_Handling;
+
+public class Q13_EH {
+    public static void main(String[] args) {
+        double x=90;
+        try{
+            double ans=(Math.cos(x)*Math.sin(x))/(Math.cos(x)+Math.sin(x));
+            System.out.println("Ans is : "+ans);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
