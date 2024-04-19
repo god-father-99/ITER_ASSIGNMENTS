8cdbbdffff58acfbea3f5fbdf08dc175fdf1619b
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 08:20:19 +0530

    Q-17 ArrayIndexOutOfBounds Exceptionwith LL

---
+++ src/Error_Handling/Q17_EH.java
@@ -0,0 +1,25 @@
+package Error_Handling;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q17_EH {
+    public static void main(String[] args) {
+        List<Integer>ll=new LinkedList<>();
+        int [] arr={1,2,3,4,5};
+        int idx=0;
+        try{
+            while(true){
+                ll.add(arr[idx]);
+                if(idx<4)
+                    System.out.print(arr[idx]+"->");
+                else
+                    System.out.println(arr[idx]);
+                idx++;
+            }
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
