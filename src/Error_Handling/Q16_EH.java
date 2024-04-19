5713eb90d0d640f7f22c545beaefc59128f83e5b
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 08:10:16 +0530

    Q-16 ArrayIndexOutOfBounds Exception

---
+++ src/Error_Handling/Q16_EH.java
@@ -0,0 +1,13 @@
+package Error_Handling;
+
+public class Q16_EH {
+    public static void main(String[] args) {
+        int [] arr=new int[5];
+        try{
+            System.out.println(arr[5]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
