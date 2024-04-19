d35402c24c52b1b6b127bb8002cfe1499194191d
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 07:52:38 +0530

    Q-14 Arithmatic Exception

---
+++ src/Error_Handling/Q14_EH.java
@@ -0,0 +1,14 @@
+package Error_Handling;
+
+public class Q14_EH {
+    public static void main(String[] args) {
+        double x=90;
+        try{
+            double ans=Math.log(Math.abs(Math.cos(x)+Math.sin(x)))/(Math.tan(x)-(1/Math.tan(x)));
+            System.out.println("Ans is : "+ans);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
