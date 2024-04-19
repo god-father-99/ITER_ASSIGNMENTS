a34dd33206c2dedbd3082f18f0da8b2ef64c4a86
god-father-99 <aditya23sahu@gmail.com>
2024-04-19 11:35:34 +0530

    Q12 Trigonometry Exception Handling

---
+++ src/CSW_4th_Sem/Error_Handling/Q12_EH.java
@@ -0,0 +1,14 @@
+package CSW_4th_Sem.Error_Handling;
+
+public class Q12_EH {
+    public static void main(String[] args) {
+        double x=90;
+        try{
+            double ans=Math.sqrt(Math.abs(Math.sin(x)*Math.cos(x)))/(Math.tan(x)+1);
+            System.out.println("ans is : "+ans);
+        }
+        catch(ArithmeticException e ){
+            System.out.println(e.getMessage());
+        }
+    }
+}
